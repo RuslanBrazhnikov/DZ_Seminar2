f1d1aa691dc1bee1a4ea7dc4192fdff3a856cb11
ruslan brazhnikov <ruslan.1995mai@gmail.com>
2022-08-11 13:55:41 +0500

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+
+int Sec(int number)
+{
+    string x = "Число не трехзначное!";
+    if (number >= 100 && number < 1000)
+    {
+        number = number / 10;
+        return number % 10;
+    }
+    else
+    {
+        Console.WriteLine("Число не трехзначное!");
+        return -1;
+    }   
+}
+
+int res = Sec(num);
+Console.Write($"Вторая цифра числа {num} ==> {res}");
---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int FuncThird(int number)
+{
+    while (num == num % -100)
+    if (num == num)
+    {
+        break;
+    }
+}
+
+int res = FuncThird(num);
+Console.WriteLine($"{res}");
